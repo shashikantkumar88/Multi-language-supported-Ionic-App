60ec0b6de473e70fd9b1777039158541ab821a98
shashikant kumar <shashikant.kumar@xpointers.com>
2014-07-15 16:27:37 +0530

    js file added
    Language js file is added

---
+++ www/lang/en.js
@@ -0,0 +1,9 @@
+var translations_en = {
+    TAB1: "Dashboard",
+    TAB2: "Friends",
+    TAB3: "Setting",
+    lang_en: 'English',
+    lang_hi: 'Hindi',
+    dash_desc: 'It provides components like filters and directives, asynchronous loading of i18n data, full pluralization support through MessageFormat and much more!',
+    back_button: 'Back'
+};
